3d5c0a1718b60a4be6210fbde7a27d6601fd5f0d
shaq <shaq0001@gmail.com>
2015-03-03 10:19:02 -0600

    added selectors

--- production/manifests/nodes.pp
+++ production/manifests/nodes.pp
@@ -1,9 +1,22 @@
-$ntpservice = $osfamily ? {
- 'redhat' => 'ntpd',
- 'debian' => 'ntp',
- 'default' => 'ntp',
-}
 node 'wiki' { 
+ class { 'linux':
+ }
+}
+node 'wikitest' { 
+ class { 'linux':
+ }
+}
+class linux {
+   file { '/info.txt':
+     ensure  => 'present',
+	 content => inline_template("Created by Puppet at <%= Time.now %>\n"),
+      }
+
+  $ntpservice = $osfamily ? {
+   'redhat' => 'ntpd',
+   'debian' => 'ntp',
+   'default' => 'ntp',
+}
 
   package { 'ntp':
    ensure => 'installed',}
@@ -13,13 +26,6 @@ node 'wiki' {
 	enable => true,
   
   }
+
 }
-node 'wikitest' { 
-  package { 'ntp':
-    ensure => 'installed',}
-  
-  service { $ntpservice:
-    ensure => 'running',
-	enable => true,
-}
-}
+
