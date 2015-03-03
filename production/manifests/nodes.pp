d6cd65a1517d2ac76fdbf390150ed937d492bbb8
shaq <shaq0001@gmail.com>
2015-03-03 08:49:07 -0600

    add node.pp file

---
+++ production/manifests/nodes.pp
@@ -0,0 +1,22 @@
+node 'wiki' { 
+  file {'/info.txt':
+    ensure  => 'present',
+	content => inline_template("Created by Puppetmaster at <%= Time.now %>\n"),
+	}
+  package { 'ntp':
+    ensure => 'installed',}
+  service { 'ntpd':
+    ensure => 'running',
+	enable => true,
+  
+  }
+
+}
+node 'wikitest' { 
+  package { 'ntp':
+    ensure => 'installed',}
+  service { 'ntp':
+    ensure => 'running',
+	enable => true,
+}
+}
